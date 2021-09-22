c91f01f189259cce6aadf3a85b72a9b07f9fa0d4
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-09-21 23:07:19 -0700

    added model show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    StockQuote::Stock.new(api_key: "pk_b75991ed5a47475da85f58ae649b58eb" )
+    StockQuote::Stock.new(api_key: "pk_932aaf347b164db49199be366904b357" )
     if params[:query] == ""
       @nothing = "Hey you forgot putting something"
     elsif params[:query]
